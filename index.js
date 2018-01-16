94279ba2f00d1406cda5329e41b689a1103a6a7e
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-01-16 01:06:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,3 +30,7 @@ function removeLastKitten(){
   var newArray = kittens.slice(0,kittens.length-1)
   return newArray;
 }
+
+function removeFirstKitten(){
+  var newArray = kitten(1,kittens.length)
+}
